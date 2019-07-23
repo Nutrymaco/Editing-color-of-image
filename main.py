a03f311f99f346ad46cbcec7b14eee02f53b9bd7
Nutrymaco <33357780+Nutrymaco@users.noreply.github.com>
2019-07-23 10:07:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PicFunc import *
-from VectorRotate import getRotatedImage
-from ImageColors3D import get3DColorMap
+from VectorRotate import get_rotated_image
+from ImageColors3D import get_3d_color_map
 
 
 config = {
@@ -24,16 +24,17 @@ if __name__ == "__main__":
             [0, 0, 2]
         ]
     )
+    original_image = parse_image(config['img_path'])
+    original_image.show()
 
-    # new_image = getChangedImage(config['img_path'], sym_matrix)
-    # new_image.save('new_mona_lisa5.jpg')
+    new_image = get_changed_image(config['img_path'], sym_matrix)
+    new_image.show()
 
-    # new_image = getChangedImage(config['img_path'], diagonal_matrix)
-    # new_image.save('diagonal_mona_lisa.jpg')
+    new_image = get_changed_image(config['img_path'], diagonal_matrix)
+    new_image.show()
 
-    new_image = getRotatedImage(config['img_path'], x_rotate=-40, y_rotate=-40, z_rotate=15)
-    new_image.save('new_mona_lisa.jpg')
+    new_image = get_rotated_image(config['img_path'], x_rotate=-40, y_rotate=-40, z_rotate=15)
+    new_image.show()
 
-    get3DColorMap(config['img_path']).show()
-    get3DColorMap('new_mona_lisa.jpg').show()
-
+    get_3d_color_map(config['img_path']).show()
+    get_3d_color_map('new_mona_lisa.jpg').show()
