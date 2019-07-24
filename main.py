80357c668f10c5b24252f65f797c45030f401b4b
Nutrymaco <33357780+Nutrymaco@users.noreply.github.com>
2019-07-24 13:48:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
             [0, 0, 2]
         ]
     )
-    original_image = parse_image(config['img_path'])
+    original_image = Image.open(config['img_path'])
     original_image.show()
 
     new_image = get_changed_image(config['img_path'], sym_matrix)
@@ -38,3 +38,4 @@ if __name__ == "__main__":
 
     get_3d_color_map(config['img_path']).show()
     get_3d_color_map('new_mona_lisa.jpg').show()
+
